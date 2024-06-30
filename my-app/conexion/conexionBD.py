c1ae60d17209ff7f62ccfea7445606036e664ee4
gloria2014 <gcastromerma@gmail.com>
2024-06-30 18:17:38 -0400

    Update conexionBD.py

--- my-app/conexion/conexionBD.py
+++ my-app/conexion/conexionBD.py
@@ -11,7 +11,7 @@ def connectionBD():
             host="localhost",
             port="3306",
             user="root",
-            passwd="Estrella.23",
+            passwd="",
             database="clientealfa2db",
             charset='utf8mb4',
             collation='utf8mb4_unicode_ci',
